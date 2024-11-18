cf5835f21c9ed52d59681a1d129caccc4d98b6bf
FarX0 <tommaso.falchi30@gmail.com>
2024-11-18 18:51:59 +0100

    fixato echo con piu argomenti e env da finire

--- src/execute_command.c
+++ src/execute_command.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execute_command.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rd-agost <rd-agost@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tfalchi <tfalchi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 14:39:48 by tfalchi           #+#    #+#             */
-/*   Updated: 2024/09/09 11:13:22 by rd-agost         ###   ########.fr       */
+/*   Updated: 2024/11/18 16:05:16 by tfalchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 
 void execute_command(t_data *data)
 {
-	if (ft_strcmp("echo", data->matrix_input[1]) == 0)
+	if (ft_strcmp("echo", data->matrix_input[0]) == 0)
 		builtin_echo(data);
-	else if (ft_strcmp("cd", data->matrix_input[1]) == 0)
+	else if (ft_strcmp("cd", data->matrix_input[0]) == 0)
 		printf("cd\n");
-	else if(ft_strcmp("pwd", data->matrix_input[1]) == 0)
+	else if(ft_strcmp("pwd", data->matrix_input[0]) == 0)
 		pwd();
-	else if (ft_strcmp("export", data->matrix_input[1]) == 0)
+	else if (ft_strcmp("export", data->matrix_input[0]) == 0)
 		export(data);
-	else if (ft_strcmp("unset", data->matrix_input[1]) == 0)
+	else if (ft_strcmp("unset", data->matrix_input[0]) == 0)
 		printf("unset\n");
-	else if (ft_strcmp("env", data->matrix_input[1]) == 0)
+	else if (ft_strcmp("env", data->matrix_input[0]) == 0)
 		print_matrix(data->env);
 }
 
--- src/export/export.c
+++ src/export/export.c
@@ -6,7 +6,7 @@
 /*   By: tfalchi <tfalchi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/30 15:39:47 by tfalchi           #+#    #+#             */
-/*   Updated: 2024/09/10 12:18:06 by tfalchi          ###   ########.fr       */
+/*   Updated: 2024/11/18 18:49:25 by tfalchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,54 +16,57 @@ void	export(t_data *data)
 {
 	int		j;
 	int		i;
+	int		aug = 1;
 	char	*substr;
 
 	// for now works only with one variable
 	j = 0;
 	i = 0;
-	if (data->matrix_input[2] == NULL) // if there are no arguments, print the environment
+	if (data->matrix_input[aug] == NULL) // if there are no arguments, print the environment
 	{
-		print_export(*data); // funzione da finire
+		print_export(*data);
 		return ;
 	}
-	while (data->matrix_input[2][j])
+	while (data->matrix_input[aug])
 	{
-		if (data->matrix_input[2][j] == '_' && data->matrix_input[2][j + 1] == '=')
+	while (data->matrix_input[aug][j])
+	{
+		if (data->matrix_input[aug][j] == '_' && data->matrix_input[aug][j + 1] == '=')
 		{
-			while (data->matrix_input[2][j] != '\0' && data->matrix_input[2][j] != ' ')
+			while (data->matrix_input[aug][j] != '\0' && data->matrix_input[aug][j] != ' ')
 				j++;
-			if (data->matrix_input[2][j] == ' ')
+			if (data->matrix_input[aug][j] == ' ')
 				j++;
 		}
 		else
 		{
-			if (ft_isdigit(data->matrix_input[2][j]) || data->matrix_input[2][j] == '=')
+			if (ft_isdigit(data->matrix_input[aug][j]) || data->matrix_input[aug][j] == '=')
 			{
-				while (data->matrix_input[2][j] != '\0' && data->matrix_input[2][j] != ' ')
+				while (data->matrix_input[aug][j] != '\0' && data->matrix_input[aug][j] != ' ')
 					j++;
 				i = j - 1;// da miodificare
-				while (data->matrix_input[2][i] != '\0' && data->matrix_input[2][i] != ' ' && i != 0)
+				while (data->matrix_input[aug][i] != '\0' && data->matrix_input[aug][i] != ' ' && i != 0)
 					i--;
-				substr = ft_substr(data->matrix_input[2], i, j - i);
+				substr = ft_substr(data->matrix_input[aug], i, j - i);
 				printf("i = %d, j = %d\n", i, j);
 				ft_printf("minishell: export: `%s': not a valid identifier\n", substr);
 			}
 			else
 			{
-				while (ft_isalnum(data->matrix_input[2][j]) || data->matrix_input[2][j] == '_')
+				while (ft_isalnum(data->matrix_input[aug][j]) || data->matrix_input[aug][j] == '_')
 					j++;
-				if (data->matrix_input[2][j] == '=')
+				if (data->matrix_input[aug][j] == '=')
 				{
 					j++;
-					while (data->matrix_input[2][j] != '\0' && data->matrix_input[2][j] != ' ')
+					while (data->matrix_input[aug][j] != '\0' && data->matrix_input[aug][j] != ' ')
 						j++;
 				}
-				if (data->matrix_input[2][j] == '\0')
+				if (data->matrix_input[aug][j] == '\0')
 				{
 					env_modification(data, j, i);
 					return ;
 				}
-				if (data->matrix_input[2][j] == ' ')
+				if (data->matrix_input[aug][j] == ' ')
 				{
 					env_modification(data, j, i);
 					j++;
@@ -72,7 +75,12 @@ void	export(t_data *data)
 			}
 		}
 	}
-	/* while (data->matrix_input[2][j] != '\0')
+	aug++;
+	printf("aug = %d\n", aug);
+	i = 0;
+	j = 0;
+	}
+	/* while (data->matrix_input[aug][j] != '\0')
 		j++; */
 	return ;
 }
@@ -85,7 +93,7 @@ void	env_modification(t_data *data, int j, int x)
 
 	i = 0;
 	strcpy = ft_calloc(sizeof(char), j - x + 1);
-	ft_strlcpy(strcpy, &data->matrix_input[2][x], j - x + 1);
+	ft_strlcpy(strcpy, &data->matrix_input[1][x], j - x + 1);
 	pos_eq = ft_strchr(strcpy, '=') - strcpy;
 	while (data->env[i] != NULL)
 	{
