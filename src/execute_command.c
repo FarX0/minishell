2f067288eaf2d173a99496f6605f624d04ca14a3
Lorenzo Fiorini <_lorenzofio_@Air-di-Lorenzo.Home>
2024-11-19 11:51:32 +0100

    Da aggiornare la libreria e rivedere se funziona

--- src/execute_command.c
+++ src/execute_command.c
@@ -3,39 +3,210 @@
 /*                                                        :::      ::::::::   */
 /*   execute_command.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rd-agost <rd-agost@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lofiorin <lofiorin@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 14:39:48 by tfalchi           #+#    #+#             */
-/*   Updated: 2024/09/09 11:13:22 by rd-agost         ###   ########.fr       */
+/*   Updated: 2024/11/18 02:25:53 by lofiorin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include <fcntl.h>
+
 
 void execute_command(t_data *data)
 {
-	if (ft_strcmp("echo", data->matrix_input[1]) == 0)
-		builtin_echo(data);
-	else if (ft_strcmp("cd", data->matrix_input[1]) == 0)
-		printf("cd\n");
-	else if(ft_strcmp("pwd", data->matrix_input[1]) == 0)
-		pwd();
-	else if (ft_strcmp("export", data->matrix_input[1]) == 0)
-		export(data);
-	else if (ft_strcmp("unset", data->matrix_input[1]) == 0)
-		printf("unset\n");
-	else if (ft_strcmp("env", data->matrix_input[1]) == 0)
-		print_matrix(data->env);
+    int i = 0;
+    int fd_in = 0;
+    int fd_out = 1;
+    
+    while (data->matrix_input[i] != NULL)
+    {
+        if (ft_strcmp(data->matrix_input[i], "|") == 0)
+        {
+            pipex(data, i, fd_in, fd_out);
+            break;
+        }
+        else if (ft_strcmp(data->matrix_input[i], ">") == 0)
+        {
+            handle_redirection(data, i, '>');
+            break;
+        }
+        else if (ft_strcmp(data->matrix_input[i], "<") == 0)
+        {
+            handle_redirection(data, i, '<');
+            break;
+        }
+        else if (ft_strcmp(data->matrix_input[i], ">>") == 0)
+        {
+            handle_redirection(data, i, '>');
+            break;
+        }
+        else if (ft_strcmp(data->matrix_input[i], "<<") == 0)
+        {
+            handle_here_doc(data, i);
+            break;
+        }
+        else
+        {
+            // Normal command execution
+            execute_normal_command(data);
+            break;
+        }
+        i++;
+    }
 }
 
-void	print_matrix(char **matrix)
+// Handle normal command execution
+void execute_normal_command(t_data *data)
 {
-	int i;
+    if (ft_strcmp("echo", data->matrix_input[0]) == 0)
+        builtin_echo(data);
+    else if (ft_strcmp("cd", data->matrix_input[0]) == 0)
+        builtin_cd(data);
+    else if (ft_strcmp("pwd", data->matrix_input[0]) == 0)
+        builtin_pwd();
+    else if (ft_strcmp("export", data->matrix_input[0]) == 0)
+        builtin_export(data);
+    else if (ft_strcmp("unset", data->matrix_input[0]) == 0)
+        builtin_unset(data);
+    else if (ft_strcmp("env", data->matrix_input[0]) == 0)
+        builtin_env(data);
+    else
+        execve_command(data);
+}
 
-	i = 0;
-	while (matrix[i] != NULL)
-	{
-		printf("%s\n", matrix[i]);
-		i++;
-	}
+// Handle the redirections: >, <, >>, <<
+void handle_redirection(t_data *data, int i, char redir)
+{
+    int fd;
+    
+    if (redir == '>' || redir == '>>')
+    {
+        if (redir == '>')
+            fd = open(data->matrix_input[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        else
+            fd = open(data->matrix_input[i + 1], O_WRONLY | O_CREAT | O_APPEND, 0644);
+
+        if (fd == -1)
+        {
+            perror("minishell: open");
+            return;
+        }
+        dup2(fd, STDOUT_FILENO);
+        close(fd);
+    }
+    else if (redir == '<')
+    {
+        fd = open(data->matrix_input[i + 1], O_RDONLY);
+
+        if (fd == -1)
+        {
+            perror("minishell: open");
+            return;
+        }
+        dup2(fd, STDIN_FILENO);
+        close(fd);
+    }
+    else if (redir == '>')
+    {
+        handle_here_doc(data, i);
+    }
+}
+
+// Handle here-doc redirection (<<)
+void handle_here_doc(t_data *data, int i)
+{
+    char *line;
+    int fd;
+
+    fd = open(".heredoc_tmp", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (fd == -1)
+    {
+        perror("minishell: open");
+        return;
+    }
+
+    while (1)
+    {
+        line = readline("> ");
+        if (!line || ft_strcmp(line, data->matrix_input[i + 1]) == 0)
+        {
+            free(line);
+            break;
+        }
+        write(fd, line, ft_strlen(line));
+        write(fd, "\n", 1);
+        free(line);
+    }
+    close(fd);
+
+    fd = open(".heredoc_tmp", O_RDONLY);
+    if (fd == -1)
+    {
+        perror("minishell: open");
+        return;
+    }
+
+    dup2(fd, STDIN_FILENO);
+    close(fd);
+}
+
+// Pipe handler for commands with pipes
+void pipex(t_data *data, int i, int fd_in, int fd_out)
+{
+    int pipe_fd[2];
+    pid_t pid;
+
+    pipe(pipe_fd);
+
+    pid = fork();
+    if (pid == 0)
+    {
+        // First process
+        if (fd_in != 0)
+            dup2(fd_in, STDIN_FILENO);
+        if (fd_out != 1)
+            dup2(fd_out, STDOUT_FILENO);
+
+        close(pipe_fd[0]);
+
+        // Execute the command before the pipe
+        execve_command(data);
+    }
+    else if (pid > 0)
+    {
+        // Parent process
+        waitpid(pid, NULL, 0);
+        close(pipe_fd[1]);
+        fd_in = pipe_fd[0];
+    }
+}
+
+// Execute the command using execve
+void execve_command(t_data *data)
+{
+    pid_t pid;
+    char *path;
+
+    pid = fork();
+    if (pid == 0)
+    {
+        // Convert matrix_input to path and arguments
+        path = get_path(data->matrix_input[0]);
+
+        if (execve(path, data->matrix_input, data->env) == -1)
+        {
+            perror("minishell");
+            exit(1);
+        }
+    }
+    else if (pid > 0)
+    {
+        waitpid(pid, NULL, 0);
+    }
+    else
+    {
+        perror("fork failed");
+    }
 }
--- src/parsing.c
+++ src/parsing.c
@@ -3,135 +3,161 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tfalchi <tfalchi@student.42.fr>            +#+  +:+       +#+        */
+/*   By: lofiorin <lofiorin@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 11:48:19 by tfalchi           #+#    #+#             */
-/*   Updated: 2024/11/15 17:16:45 by tfalchi          ###   ########.fr       */
+/*   Updated: 2024/11/18 02:23:09 by lofiorin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+// Function to parse input and split on pipes, redirections
 t_data parsing(t_data data)
 {
-	data = del_extra_spaces(data);
-	data = split_input(data);
-	return (data);
+    data = del_extra_spaces(data);  // Remove extra spaces
+    data = split_input(data);  // Split input by pipes and redirections
+    return (data);
 }
 
+// Function to remove unnecessary spaces outside of quotes
 t_data del_extra_spaces(t_data data)
 {
-	int i;
-	int j;
-	char *strcpy;
+    int i = 0;
+    int j = 0;
+    char *strcpy;
 
-	i = 0;
-	j = 0;
-	data.input = ft_strdup(data.original_input);
-	if (data.input)
-		strcpy = ft_calloc(sizeof(char), ft_strlen(data.input) + 1);
-	while (data.input[i] == ' ')
-		i++;
-	while (data.input[i] != '\0')
-	{
-		if (data.input[i] == 39 && data.flag1 % 2 == 0)
-			data.flag2 += 1;
-		if (data.input[i] == 34 && data.flag2 % 2 == 0)
-			data.flag1 += 1; 
-		while (data.input[i] == ' ' && data.input[i + 1] == ' ' && data.flag1 % 2 == 0 && data.flag2 % 2 == 0)
-			i++;
-		if (data.input[i] == ' ' && data.flag1 % 2 == 0 && data.flag2 % 2 == 0 && data.input[i + 1] == '\0')
-			i++;
-		strcpy[j] = data.input[i];
-		i++;
-		j++;
-	}
-	free(data.input);
-	data.input = strcpy;
-	return (data);
+    data.input = ft_strdup(data.original_input);
+    if (data.input)
+        strcpy = ft_calloc(sizeof(char), ft_strlen(data.input) + 1);
+
+    while (data.input[i] == ' ')  // Skip leading spaces
+        i++;
+
+    while (data.input[i] != '\0')
+    {
+        // Handle single and double quotes
+        if (data.input[i] == 39 && data.flag1 % 2 == 0)
+            data.flag2 += 1;
+        if (data.input[i] == 34 && data.flag2 % 2 == 0)
+            data.flag1 += 1;
+
+        // Skip extra spaces when not inside quotes
+        while (data.input[i] == ' ' && data.input[i + 1] == ' ' && data.flag1 % 2 == 0 && data.flag2 % 2 == 0)
+            i++;
+
+        if (data.input[i] == ' ' && data.flag1 % 2 == 0 && data.flag2 % 2 == 0 && data.input[i + 1] == '\0')
+            i++;
+
+        strcpy[j++] = data.input[i++];
+    }
+
+    free(data.input);
+    data.input = strcpy;
+
+    return data;
 }
 
+// Updated to handle pipes and redirections
 t_data split_input(t_data data)
 {
-	int i;
-	int j;
-	int k;
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    int in_quotes = 0;
 
-	i = 0;
-	j = 0;
-	k = 0;
-	if (data.input)
-		data.matrix_input = matrix_alloc(data.input);
-	while (data.input[i] != '\0')
-	{
-		if (data.input[i] == 39) //da fare on un &
-		{
-			i++;
-			while (data.input[i] != 39 && data.input[i] != '\0')
-			{
-				data.matrix_input[k][j] = data.input[i];
-				i++;
-				k++;
-			}
-			i++;
-		}
-		if (data.input[i] == 34)
-		{
-			i++;
-			while (data.input[i] != 34 && data.input[i] != '\0')
-			{
-				data.matrix_input[k][j] = data.input[i];
-				i++;
-				k++;
-			}
-			i++;
-		}
-		if (data.input[i] == ' ')//fattibile con split
-		{
-			data.matrix_input[k][j] = '\0';
-			j = 0;
-			k++;
-			i++;
-		}
-		if (data.input[i] == '\0')
-			break;
-		data.matrix_input[k][j] = data.input[i];
-		i++;
-		j++;
-	}
-	return (data);
+    if (data.input)
+        data.matrix_input = matrix_alloc(data.input);
+
+    while (data.input[i] != '\0')
+    {
+        // Skip spaces outside of quotes
+        if (data.input[i] == 39 && in_quotes % 2 == 0) // Single quote
+            in_quotes = !in_quotes;
+        else if (data.input[i] == 34 && in_quotes == 0) // Double quote
+            in_quotes = !in_quotes;
+
+        if ((data.input[i] == ' ' && !in_quotes) || data.input[i] == '\t') // Skip spaces
+        {
+            i++;
+            continue;
+        }
+
+        // Handle redirection operators: <, >, <<, >>
+        if (data.input[i] == '>' && data.input[i + 1] == '>')
+        {
+            data.matrix_input[k][j++] = '>';
+            data.matrix_input[k][j++] = '>';
+            i += 2;
+        }
+        else if (data.input[i] == '<' && data.input[i + 1] == '<')
+        {
+            data.matrix_input[k][j++] = '<';
+            data.matrix_input[k][j++] = '<';
+            i += 2;
+        }
+        else if (data.input[i] == '>' || data.input[i] == '<')
+        {
+            data.matrix_input[k][j++] = data.input[i++];
+        }
+        else if (data.input[i] == '|')  // Pipe operator
+        {
+            data.matrix_input[k][j] = '\0';
+            k++;
+            j = 0;
+            data.matrix_input[k][j++] = '|';
+            i++;
+        }
+        else
+        {
+            // Regular characters, add to current argument
+            data.matrix_input[k][j++] = data.input[i++];
+        }
+
+        // End current argument when space is encountered outside of quotes
+        if ((data.input[i] == ' ' || data.input[i] == '\0') && !in_quotes)
+        {
+            data.matrix_input[k][j] = '\0';
+            k++;
+            j = 0;
+        }
+    }
+
+    return data;
 }
 
+// Function to allocate memory for a matrix to hold the parsed input
 char **matrix_alloc(char *str)
 {
-	int i, j, k;
-	char **matrix;
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    char **matrix;
 
-	i = 0;
-	j = 0;
-	k = 0;
-	while (str[i] != '\0')
-	{
-		if (str[i] == ' ')
-			k++;
-		i++;
-	}
-	matrix = ft_calloc(sizeof(char *), k + 2);
-	i = 0;
-	k = 0;
-	while (str[i] != '\0')
-	{
-		while (str[i] != ' ' && str[i] != '\0')
-		{
-			i++;
-			j++;
-		}
-		matrix[k] = ft_calloc(sizeof(char), j + 1);
-		k++;
-		j = 0;
-		if (str[i] == '\0')
-			break;
-		i++;
-	}
-	return (matrix);
+    while (str[i] != '\0')
+    {
+        if (str[i] == ' ')
+            k++;
+        i++;
+    }
+
+    matrix = ft_calloc(sizeof(char *), k + 2);
+    i = 0;
+    k = 0;
+
+    while (str[i] != '\0')
+    {
+        while (str[i] != ' ' && str[i] != '\0')
+        {
+            i++;
+            j++;
+        }
+        matrix[k] = ft_calloc(sizeof(char), j + 1);
+        k++;
+        j = 0;
+        if (str[i] == '\0')
+            break;
+        i++;
+    }
+    return matrix;
 }
